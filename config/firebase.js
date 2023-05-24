cddba45ec5183065dc7e4792da0e057ceed96f8e
Nathan <nathan.arriesgado0g@gmail.com>
2023-05-25 00:16:04 +0800

    [Feat][Nathan] Initialized config folder and firebase configuration

---
+++ config/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC9A3bcS7EcAYUggeew1YvNk-wCNnu5A3I",
+  authDomain: "dormit-a2649.firebaseapp.com",
+  projectId: "dormit-a2649",
+  storageBucket: "dormit-a2649.appspot.com",
+  messagingSenderId: "823056166239",
+  appId: "1:823056166239:web:cb193e32e0901eeed3daea",
+  measurementId: "G-W15NPVRFCV"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
