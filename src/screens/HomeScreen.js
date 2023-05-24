50db6debf218fbfbad5b93fef5d9bcfce08826bb
Nathan <nathan.arriesgado0g@gmail.com>
2023-05-25 00:42:20 +0800

    [Misc][Nathan] Created components folder and moved components and screens under src

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View>
+        <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+
+});
---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const LoginScreen = () => {
+  return (
+    <View>
+        <Text>Login Screen</Text>
+    </View>
+  );
+}
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+
+});
