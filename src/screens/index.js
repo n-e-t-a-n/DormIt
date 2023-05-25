b9841d841e4eadf46e24aa099ecbd8640371cc0d
Nathan <nathan.arriesgado0g@gmail.com>
2023-05-25 17:33:28 +0800

    [Feat][Nathan] Initialized screens for application

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,2 +1,3 @@
-export { default as HomeScreen } from "./HomeScreen";
-export { default as LoginScreen } from "./LoginScreen";
+export { default as Home } from "./Home";
+export { default as Login } from "./Login";
+export { default as Welcome } from "./Welcome";
