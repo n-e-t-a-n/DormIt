23dab5d7a323418cfc671504dc983d33204bd569
Nathan <nathan.arriesgado0g@gmail.com>
2023-05-27 00:29:53 +0800

    [Misc][Nathan] Removed unused imports in getAuthenticatedUser

--- src/utils/hooks/getAuthenticatedUser.js
+++ src/utils/hooks/getAuthenticatedUser.js
@@ -1,6 +1,5 @@
-import React from 'react';
 import app from '../../../config/firebase';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 
 const auth = getAuth(app);
 
