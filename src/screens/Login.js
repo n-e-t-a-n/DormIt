cfec83d3805ba8b342d0a8b0c18b04e250b194ff
Nathan <nathan.arriesgado0g@gmail.com>
2023-05-27 21:15:17 +0800

    [Feat][Nathan] Added temporary styling for login and register screens

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -3,8 +3,9 @@ import { auth } from '../../config/firebase';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { StyleSheet, 
          View, 
+         Text, 
          TextInput, 
-         Button } from 'react-native';
+         Pressable } from 'react-native';
 
 export default function Login({ navigation }) {
   const [email, setEmail] = useState('');
@@ -25,16 +26,25 @@ export default function Login({ navigation }) {
   return (
     <View style={styles.container}>
       <TextInput
+        style={styles.input}
         placeholder="Email"
         onChangeText={(text) => {setEmail(text)}}
       />
+
       <TextInput
+        style={styles.input}
         placeholder="Password"
         secureTextEntry
         onChangeText={(text) => {setPassword(text)}}
       />
-      <Button title="Login" onPress={handleLogin} />
-      <Button title="Register" onPress={() => navigation.navigate("Register")} />
+
+      <Pressable style={[styles.button, styles.loginButton]} onPress={handleLogin}> 
+        <Text style={styles.text}>Login</Text>
+      </Pressable>
+
+      <Pressable style={[styles.button, styles.registerButton]} onPress={() => navigation.navigate("Register")}> 
+        <Text style={styles.text}>Register</Text>
+      </Pressable>
     </View>
   );
 }
@@ -43,7 +53,39 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    justifyContent: 'center',
+    paddingHorizontal: 40,
+  },
+  input: {
+    height: 50,
+    width: '100%',
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+  },
+  button: {
     alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+  },
+  loginButton : {
+    marginBottom: 5,
+    backgroundColor: '#000000',
+    marginBottom: 3,
+  },
+  registerButton : {
+    backgroundColor: '#000000',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
   },
 });
--- src/screens/Register.js
+++ src/screens/Register.js
@@ -3,8 +3,9 @@ import { auth } from '../../config/firebase';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { StyleSheet, 
          View, 
+         Text, 
          TextInput, 
-         Button,
+         Pressable,
          ToastAndroid } from 'react-native';
 
 export default function Login({ navigation }) {
@@ -38,16 +39,23 @@ export default function Login({ navigation }) {
   return (
     <View style={styles.container}>
       <TextInput
+        style={styles.input}
         placeholder="Email"
         onChangeText={(text) => setEmail(text)}
       />
       <TextInput
+        style={styles.input}
         placeholder="Password"
         secureTextEntry
         onChangeText={(text) => setPassword(text)}
       />
-      <Button title="Sign In" onPress={handleRegister} />
-      <Button title="Cancel" onPress={() => navigation.goBack()} />
+      <Pressable style={[styles.button, styles.registerButton]} onPress={handleRegister}> 
+        <Text style={styles.text}>Register</Text>
+      </Pressable>
+
+      <Pressable style={[styles.button, styles.cancelButton]} onPress={() => navigation.goBack()}> 
+        <Text style={styles.text}>Cancel</Text>
+      </Pressable>
     </View>
   );
 }
@@ -56,7 +64,39 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    justifyContent: 'center',
+    paddingHorizontal: 40,
+  },
+  input: {
+    height: 50,
+    width: '100%',
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+  },
+  button: {
     alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+  },
+  registerButton : {
+    marginBottom: 5,
+    backgroundColor: '#000000',
+    marginBottom: 3,
+  },
+  cancelButton : {
+    backgroundColor: '#000000',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
   },
 });
