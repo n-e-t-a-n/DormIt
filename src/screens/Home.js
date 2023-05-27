7ab82efe38155ac14ae05afa759cf825b7ae5a17
Nathan <nathan.arriesgado0g@gmail.com>
2023-05-27 23:16:35 +0800

    [Feat][Nathan] Created logout functionality

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { getUser } from '../utils';
+import { StyleSheet, 
+         Text, 
+         View,
+         Pressable } from 'react-native';
+import { homeStyles } from '../styles';
+import { getAuth, signOut } from 'firebase/auth';
 
 export default function Home() {
+  const handleLogout = () => {
+    const auth = getAuth();
+    signOut(auth).then(() => {
+      
+    }).catch((error) => {});
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Home Screen!</Text>
+      <Text>{getUser().email}</Text>
       <StatusBar style="auto" />
+
+      <Pressable style={[homeStyles.button, homeStyles.logoutButton]} onPress={handleLogout}> 
+        <Text style={homeStyles.text}>Logout</Text>
+      </Pressable>
     </View>
   );
 }
