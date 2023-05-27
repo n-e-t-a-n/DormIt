3df14b44d4d96bcddc5d850991aa182c8c4dfd19
Nathan <nathan.arriesgado0g@gmail.com>
2023-05-27 12:52:03 +0800

    [Fix][Nathan] Renamed auth pages and configured stack navigation

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,25 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
+import { auth } from '../../config/firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function Login({ navigation }) {
-  const handleSignIn = async () => {
-
-  };
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   return (
     <View style={styles.container}>
       <TextInput
         placeholder="Email"
-        onChangeText={() => {}}
+        onChangeText={(text) => setEmail(text)}
       />
       <TextInput
         placeholder="Password"
         secureTextEntry
-        onChangeText={() => {}}
+        onChangeText={(text) => setPassword(text)}
       />
-      <Button title="Login" onPress={handleSignIn} />
-      <Button title="Register" onPress={() => navigation.navigate("Register")} />
+      <Button title="Sign In" onPress={() => {}} />
+      <Button title="Cancel" onPress={() => navigation.pop(1)} />
     </View>
   );
 }
--- src/stacks/Auth.js
+++ src/stacks/Auth.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import { Welcome, Login } from '../screens';
+import { Login, Register } from '../screens';
 
 const Stack = createStackNavigator();
 
 export default function Auth() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="Register" component={Register} />
       </Stack.Navigator>
     </NavigationContainer>
   );
