039200dde4adcbc39b51cefc0036f43b898a0c49
Nathan <nathan.arriesgado0g@gmail.com>
2023-05-28 00:02:59 +0800

    [Fix][Nathan] Removed ciruclar import dependency and maximum call stack errors

--- src/stacks/User.js
+++ src/stacks/User.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import { getUser } from '../utils';
 import { Home } from '../screens';
 
 const UserStack = createStackNavigator();
 
 export default function User() {
+  if (!getUser()) navigation.goBack();
+
   return (
     <UserStack.Navigator>
       <UserStack.Screen name="Home" component={Home} />
