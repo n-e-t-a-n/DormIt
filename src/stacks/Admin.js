cd023bb9d5a451cec39d8e6da7ba4755dbb3ad51
Nathan <nathan.arriesgado0g@gmail.com>
2023-05-31 01:47:05 +0800

    [Feat][Nathan] Initialied home screen for admin role

--- src/stacks/Admin.js
+++ src/stacks/Admin.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Home } from '../screens/admin';
 import { auth } from '../../config/firebase';
 
 const AdminStack = createStackNavigator();
@@ -9,6 +10,7 @@ export default function Admin() {
 
   return (
     <AdminStack.Navigator>
+      <AdminStack.Screen name="Home" component={Home} />
     </AdminStack.Navigator>
   );
 }
