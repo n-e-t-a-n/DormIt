f7efdd6d4b0d39f6093accb2fcd8a8980b1679bd
Nathan <nathan.arriesgado0g@gmail.com>
2023-05-30 13:22:42 +0800

    [Feat][Nathan] Created storeUser function to store user in firestore after registration

---
+++ src/utils/database/storeUser.js
@@ -0,0 +1,18 @@
+import { db } from '../../../config/firebase';
+import { doc, setDoc } from 'firebase/firestore';
+
+export default async function storeOwner({ email }, roleType) {
+    try {
+        await setDoc(
+            doc(db, "users", email),
+            {
+                first_name: "test",
+                last_name: "account",
+                role: roleType,
+                gender: "male",
+            }
+        );
+    } catch (error) {
+        console.log(error);
+    }
+}
