555171481181b9d538506ef811f9e24ccc4d86ec
Nathan <nathan.arriesgado0g@gmail.com>
2023-05-30 14:06:24 +0800

    [Misc][Nathan] Corrected typography error in documentation

--- src/utils/database/index.js
+++ src/utils/database/index.js
@@ -1,12 +1,12 @@
 export { default as getUser } from './getUser';
 /*
     Input: Nothing.
-    Output: Object with data of current logged-in user if exists, null if not, console-logged error if caught.
+    Output: Returns an Object with current user's data, or null if it doesn't exist, console-logged error if caught.
 
     E.G.
     const user = getUser();
     console.log(user);
-    => { email: sample@email.com, gender: female, age: 22 }
+    => { "email": "sample@email.com", "gender": "female", "age": 22 }
 
     Note: This is an asynchronous function. Do not forget to await it.
 */
