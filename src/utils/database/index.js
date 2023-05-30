184fbf690ae59500294ca07741bdc8638e0f1ed4
Nathan <nathan.arriesgado0g@gmail.com>
2023-05-30 13:42:23 +0800

    [Misc][Nathan] Added documentation to utility functions

--- src/utils/database/index.js
+++ src/utils/database/index.js
@@ -1,2 +1,28 @@
 export { default as getUser } from './getUser';
+/*
+    Input: Nothing.
+    Output: Object with data of current logged-in user if exists, null if not, console-logged error if caught.
+
+    E.G.
+    const user = getUser();
+    console.log(user);
+    => { email: sample@email.com, gender: female, age: 22 }
+
+    Note: This is an asynchronous function. Do not forget to await it.
+*/
+
 export { default as storeUser } from './storeUser';
+/*
+    Input: Object with data of user to-be-stored, String representing role (user || owner).
+    Output: Stores user data in firestore, with { email } as document-id, console-logged error if caught.
+    
+    E.G. 
+    const user = {
+        email: "sample@email.com",
+        gender: "female",
+        age: 22
+    };
+    storeUser(user, "owner");
+
+    Note: This is an asynchronous function. Do not forget to await it.
+*/
--- src/utils/helpers/index.js
+++ src/utils/helpers/index.js
@@ -1,2 +1,20 @@
 export { default as createToastShort } from './createToastShort';
+/*
+    Input: String representing message.
+    Output: Displays a short-length toast message on Android.
+
+    E.G.
+    const message = "This is a short toast message";
+    createToastShort(message);
+*/
+
+
 export { default as createToastLong } from './createToastLong';
+/*
+    Input: String representing message.
+    Output: Displays a long-length toast message on Android.
+
+    E.G.
+    const message = "This is a long toast message";
+    createToastLong(message);
+*/
