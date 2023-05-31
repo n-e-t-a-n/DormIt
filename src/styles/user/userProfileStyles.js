7ee62ef552b062667049d3f1375f0997a3a63b15
Sealiosaurus <sealsplat@gmail.com>
2023-06-01 02:07:25 +0800

    [Feat][Seal] Initialized simple styles for newly added screens

---
+++ src/styles/user/userProfileStyles.js
@@ -0,0 +1,46 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        padding: 20
+        
+    },
+    input: {
+        height: 50,
+        width: '100%',
+        marginBottom: 10,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 5,
+        paddingHorizontal: 10,
+    },
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 32,
+        borderRadius: 4,
+        elevation: 3,
+    },
+    logoutButton : {
+        marginTop: 3,
+        backgroundColor: '#000000',
+        marginBottom: 3,
+    },
+    email: {
+        fontSize: 16,
+        lineHeight: 21,
+        fontWeight: 'bold',
+        letterSpacing: 0.25,
+    },
+    text: {
+        fontSize: 16,
+        lineHeight: 21,
+        fontWeight: 'bold',
+        letterSpacing: 0.25,
+        color: 'white',
+    },
+});
+  
