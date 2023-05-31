a1cd36c58f9fd878efa379c7c7623070181101b6
Sealiosaurus <sealsplat@gmail.com>
2023-06-01 02:13:46 +0800

    [Config][Seal] Modified user screen index to support new screens

--- src/screens/user/index.js
+++ src/screens/user/index.js
@@ -1,1 +1,4 @@
 export { default as Home } from './Home';
+export { default as UserProfile } from './UserProfile';
+export { default as Search } from './Search';
+export { default as DormDetails } from './DormDetails';
