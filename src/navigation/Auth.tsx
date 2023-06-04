61be3811f52c99e00b920df882b3ee3037a04e28
Nathan <nathan.arriesgado0g@gmail.com>
2023-06-04 12:49:49 +0800

    [Fix][Nathan] Removed access of Admin, Owner, and User stacks from Auth

--- src/navigation/Auth.tsx
+++ src/navigation/Auth.tsx
@@ -3,9 +3,6 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import { Login, Register } from "@components/screens/auth";
-import Admin from "@navigation/Admin";
-import Owner from "@navigation/Owner";
-import User from "@navigation/User";
 
 import type { AuthStackParamList } from "@@types/navigation/Auth";
 
@@ -16,10 +13,6 @@ export default function Auth() {
     <AuthStack.Navigator initialRouteName="Login">
       <AuthStack.Screen name="Login" component={Login} />
       <AuthStack.Screen name="Register" component={Register} />
-
-      <AuthStack.Screen name="AdminStack" component={Admin} options={{ headerShown: false }} />
-      <AuthStack.Screen name="OwnerStack" component={Owner} options={{ headerShown: false }} />
-      <AuthStack.Screen name="UserStack" component={User} options={{ headerShown: false }} />
     </AuthStack.Navigator>
   );
 }
