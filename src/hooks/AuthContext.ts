ab3322a0de10c7710d4a76700a03f1f512012e6f
Nathan <nathan.arriesgado0g@gmail.com>
2023-06-05 07:19:15 +0800

    [Fix][Nathan] Added types to useIfNotMounted hook

--- src/hooks/AuthContext.ts
+++ src/hooks/AuthContext.ts
@@ -1,11 +1,11 @@
-import { useEffect, useRef } from "react";
+import { EffectCallback, useEffect, useRef } from "react";
 
 import { createContext } from "react";
 import { User } from "firebase/auth";
 
 export const UserContext = createContext<User | null | undefined>(null);
 
-export function useIfNotMounted(fn: any, inputs:any) {
+export function useIfNotMounted(fn: EffectCallback, inputs: ReadonlyArray<any>): void {
   const hasMounted = useRef(false);
 
   useEffect(() => {
