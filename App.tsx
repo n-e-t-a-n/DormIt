a16df17c06e5e0743c21e7629638bccb30952288
Nathan <nathan.arriesgado0g@gmail.com>
2023-06-04 12:43:35 +0800

    [Fix][Nathan] Entrypoint now uses useAuthState hook to determine base stack

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 
-import Auth from "@navigation/Auth";
 import { NavigationContainer } from "@react-navigation/native";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+import Auth from "@navigation/Auth";
+import RoleResolver from "@navigation/RoleResolver";
+
+import { auth } from "@config/firebase";
 
 function App() {
-  return (
-    <NavigationContainer>
-      <Auth />
-    </NavigationContainer>
-  );
+  const [user] = useAuthState(auth);
+
+  return <NavigationContainer>{user ? <RoleResolver user={user} /> : <Auth />}</NavigationContainer>;
 }
 
 export default App;
