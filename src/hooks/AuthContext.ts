80c03062a9b9733c68fd9f7b7b459eda148cd109
Nathan <nathan.arriesgado0g@gmail.com>
2023-06-05 07:16:12 +0800

    [Feat][Nathan] Created custom hook useIfNotMounted

--- src/hooks/AuthContext.ts
+++ src/hooks/AuthContext.ts
@@ -1,4 +1,17 @@
+import { useEffect, useRef } from "react";
+
 import { createContext } from "react";
 import { User } from "firebase/auth";
 
 export const UserContext = createContext<User | null | undefined>(null);
+
+export function useIfNotMounted(fn: any, inputs:any) {
+  const hasMounted = useRef(false);
+
+  useEffect(() => {
+    if (hasMounted.current) { 
+    return fn();
+    }
+    hasMounted.current = true;
+  }, inputs);
+}
