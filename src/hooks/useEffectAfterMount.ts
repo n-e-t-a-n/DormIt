6e85b600af6f5654e9b58526043b46d12dd18658
Nathan <nathan.arriesgado0g@gmail.com>
2023-06-05 23:21:04 +0800

    [Misc][Nathan] Removed unecessary auth context and renamed hook to useEffectAfterMount

---
+++ src/hooks/useEffectAfterMount.ts
@@ -0,0 +1,12 @@
+import { EffectCallback, useEffect, useRef } from "react";
+
+export default function useEffectAfterMount(fn: EffectCallback, inputs: ReadonlyArray<any>): void {
+  const hasMounted = useRef(false);
+
+  useEffect(() => {
+    if (hasMounted.current) {
+      return fn();
+    }
+    hasMounted.current = true;
+  }, inputs);
+}
