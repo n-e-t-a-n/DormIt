6aa682b59228efe29522a29e9337961f3429765d
Nathan <nathan.arriesgado0g@gmail.com>
2023-06-08 01:06:50 +0800

    [Fix][Nathan] Added explicit return type to useFirestoreState

--- src/hooks/useFirestoreState.ts
+++ src/hooks/useFirestoreState.ts
@@ -1,9 +1,9 @@
-import { useDocument } from "react-firebase-hooks/firestore";
-import { doc } from "firebase/firestore";
+import { DocumentHook, useDocument } from "react-firebase-hooks/firestore";
+import { DocumentData, doc } from "firebase/firestore";
 import { User } from "firebase/auth";
 
 import { db } from "@config/firebase";
 
-export default function useFirestoreState(user: User | undefined | null) {
+export default function useFirestoreState(user: User | undefined | null): DocumentHook<DocumentData> {
   return useDocument(doc(db, "users", user?.email || "doesnotexist"));
 }
